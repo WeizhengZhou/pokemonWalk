4956047e4c5febf003da56f77f5f69ab77ded013
Weizheng <zhou66.ustcz@gmail.com>
2016-07-22 13:22:13 -0700

    m

---
+++ pokemon/pokemon/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  pokemon
+//
+//  Created by Weizheng on 7/22/16.
+//  Copyright © 2016 Weizheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ pokemon/pokemon/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  pokemon
+//
+//  Created by Weizheng on 7/22/16.
+//  Copyright © 2016 Weizheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
